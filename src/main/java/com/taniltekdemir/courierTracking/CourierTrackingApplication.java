d05c32f8d05db11440af9e2486e23b112267cad2
Teyfik Anıl TEKDEMİR <taniltekdemir@gmail.com>
2024-12-07 09:31:09 +0300

    initial project

---
+++ src/main/java/com/taniltekdemir/courierTracking/CourierTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.taniltekdemir.courierTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taniltekdemir/courierTracking/CourierTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taniltekdemir.courierTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
